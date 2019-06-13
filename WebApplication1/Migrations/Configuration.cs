37859d451605d83ab7db2f87f52eccb7cfed9d04
Mateusz Powroźnik <mateusz.powroznik@outlook.com>
2019-06-13 14:11:55 +0200

    Aktualizacja metody Seed w klasie Configuration w folderze Migrations

--- WebApplication1/Migrations/Configuration.cs
+++ WebApplication1/Migrations/Configuration.cs
@@ -4,16 +4,21 @@ namespace WebApplication1.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using WebApplication1.DAL;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WebApplication1.DAL.KursyContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+            ContextKey = "WebApplication1.DAL.KursyContext";
         }
 
         protected override void Seed(WebApplication1.DAL.KursyContext context)
         {
+
+            KursyInitializer.SeedKursyData(context);
+            KursyInitializer.SeedUzytkownicy(context);
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
